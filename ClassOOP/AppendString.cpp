4f7f9319355c6b3dd428628b1b6f28de879ff8ea
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-04 20:04:27 -0700

    Append String Program
    Test using the .append() function in C++.

---
+++ ClassOOP/AppendString.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  StringExercise03
+//
+//  Created by brian on 10/10/22.
+//  program test using the 'strcat()' func
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string front = "wait", back = "ing";
+    cout << front.append(back) << endl;
+    
+    
+    return 0;
+}
